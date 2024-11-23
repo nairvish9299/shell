6dad03dbbfbf91e6dab50aeebad66272d9bf2c5b
vish <vish.shetty@dev.com>
2024-11-22 21:00:25 -0500

    This is my first commit No3

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -29,7 +29,7 @@ ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 mkdir ./data/processed ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp .data/raw/*server*.log .data/processed/server_logs/
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 cp ./data/processed/raw/*user*.log ./data/processed/user_logs/
 cp ./data/processed/raw/*event*.log ./data/processed/event_logs/
@@ -38,9 +38,9 @@ rm ./data/raw/*ipaddr*.log
 rm ./data/processed/user_logs/*ipaddr*.log
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/processed/inventory.txt
-ls .data/processed/user_logs/>>./data/inventory.txt 
-ls .data/processed/server_logs/>>./data/inventory.txt
-ls .data/processed/server_logs/>>./data/inventory.txt
+ls ./data/processed/user_logs/>>./data/inventory.txt 
+ls ./data/processed/server_logs/>>./data/inventory.txt
+ls ./data/processed/server_logs/>>./data/inventory.txt
 
 ###########################################
 
